86cf8a580a09c2c6df9712866f072f92340e7c67
richmondnkrumah <richmondnkrumah04@gmail>
2024-01-04 21:02:35 +0000

    added languages constant

---
+++ src/constant/Languages.ts
@@ -0,0 +1,16 @@
+const LANGUAGES: {name: string, fullName: string}[] = [
+    {
+        name: 'js',
+        fullName: 'JavaScript'
+    },
+    {
+        name: 'css',
+        fullName: 'CSS'
+    },
+    {
+        name: 'html',
+        fullName: 'HTML'
+    }
+
+]
+export default LANGUAGES
