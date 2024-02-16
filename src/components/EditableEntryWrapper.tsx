fc2eb83df9c16902d432f2ca121541a7c57ef54b
Void <richmondnkrumah04@gmail.com>
2024-02-16 10:32:59 +0000

    component wrapper for creating treenodes

---
+++ src/components/EditableEntryWrapper.tsx
@@ -0,0 +1,55 @@
+// EditableEntryWrapper.tsx
+import React, {
+  useState,
+  useRef,
+  useEffect,
+} from "react";
+import { useExplorerFileTree } from "@/store/ExplorerFileTree";
+
+interface EditableEntryWrapperProps {
+  isFolder?: boolean;
+  handleIsDone:   () => void
+}
+
+const EditableEntryWrapper: React.FC<EditableEntryWrapperProps> = ({
+  isFolder,handleIsDone
+}) => {
+  const [isEditing, setIsEditing] = useState<boolean>(true);
+  const [inputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const {createNode,setNewNode} = useExplorerFileTree()
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setIsEditing(false);
+      createNode(inputValue,isFolder)
+      handleIsDone()
+      setNewNode()
+    }
+  };
+  useEffect(() => {
+    inputRef.current?.focus({});
+  });
+
+  return (
+    isEditing && (
+      <div className="wrapper">
+        {isEditing && (
+          <input
+            ref={inputRef}
+            type="text"
+            value={inputValue}
+            onKeyDown={handleKeyDown}
+            onChange={handleInputChange}
+            disabled={!isEditing}
+          />
+        )}
+      </div>
+    )
+  );
+};
+
+export default EditableEntryWrapper;
