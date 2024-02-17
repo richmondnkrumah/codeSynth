516d9bb3d68d5d05af5d231249aab3d3044ef51c
Void <richmondnkrumah04@gmail.com>
2024-02-17 19:48:38 +0000

    ui padding fixes

--- src/app/editor/@explorer/page.tsx
+++ src/app/editor/@explorer/page.tsx
@@ -3,16 +3,13 @@ import React from "react";
 import { useThemeStore } from "@/store/Theme";
 import FileTree from '@/components/FileTree'
 
-
 const Explorer = () => {
- 
-  
   const {getTheme} = useThemeStore();
   const currentTheme = getTheme();
 
   return (
     <div
-      className={`${currentTheme.colors.secondary} h-full overflow-scroll pl-1`}
+      className={`${currentTheme.colors.secondary} h-full overflow-scroll pl-2`}
     >
       <div className="h-8">
         <h2>CodeSynth</h2>
