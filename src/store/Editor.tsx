af14cc4748b335f3af9b61128d2cb912f650109c
Void <richmondnkrumah04@gmail.com>
2024-02-17 18:40:55 +0000

    added tabHistory with CRUD

--- src/store/Editor.tsx
+++ src/store/Editor.tsx
@@ -1,29 +1,82 @@
 import { ReactElement } from "react";
 import { create } from "zustand";
+import { ICONS } from "@/constant/icons";
+
+//TODO:
+// make a property called TabHistory with every lick on a file the tab history appends to the list
+// then when addnewdeletedfile is called the the tab history removes the tab identified then updates
+// the ativeEditor to the last element of the tabhistory else null
+//TODO
 
 interface EditorFile {
   fileName: string;
   language: string;
   content: string;
   defaultValue?: string;
-  icon?: ReactElement
-  check?: string
+  icon?: ReactElement;
 }
 
 interface AllEditorFiles {
   files: undefined | null | EditorFile[];
-  currentLanguage?: string
+  deletedFiles: undefined | null | EditorFile[];
+  currentLanguage?: string;
+  activeEditor: EditorFile | null;
   addNewFile: (file: EditorFile) => void;
+  addNewDeletedFile: (file: EditorFile) => void;
   deleteFile: (file: EditorFile) => void;
-  setCurrentLanguage: (lang: string) => void; 
+  setCurrentLanguage: (lang: string) => void;
   getCurrentFiles: () => EditorFile[] | undefined | null;
   updateFile: (file: string, newContent: string) => void;
+  setActiveEditor: (editor: EditorFile) => void;
+  editorTabHistory: undefined | null | EditorFile[];
+  setNewTabHistory: (file: EditorFile) => void
 }
-
 export const useEditorFileStore = create<AllEditorFiles>()((set, get) => ({
   files: [],
+  activeEditor: null,
+  deletedFiles: [],
+  editorTabHistory: [],
+  setNewTabHistory: (file) => set(state => ({
+    editorTabHistory: [...(state.editorTabHistory?.filter(editor => editor.fileName !== file.fileName) || []) ,file]
+  })),
+  addNewDeletedFile: (file) =>
+    set((state) => ({
+      deletedFiles: state.deletedFiles ? [...state.deletedFiles, file] : [file],
+      files: state.files?.filter(
+        (oldFile) => oldFile.fileName !== file.fileName
+      ),
+      activeEditor: state.editorTabHistory?.filter(editor => editor.fileName !== file.fileName).slice(-1)[0],
+        editorTabHistory: state.editorTabHistory?.filter(editor => editor.fileName !== file.fileName)
+    })),
   addNewFile: (file) =>
-    set((state) => ({ currentLanguage:file.language, files: [...(state?.files || []), file] })),
+    set((state) => {
+      let duplicateCheck = state.files?.find(
+        (oldFile) => oldFile.fileName === file.fileName
+      );
+      let duplicateDeletedCheck = state.deletedFiles?.find(
+        (oldFile) => oldFile.fileName === file.fileName
+      );
+      if (duplicateDeletedCheck) {
+        return {
+          files: [...(state?.files || []), duplicateDeletedCheck],
+          activeEditor: file,
+          currentLanguage: file.language,
+        };
+      }
+      if (duplicateCheck) {
+        return {
+          files: [...(state?.files || [])],
+          activeEditor: file,
+          currentLanguage: file.language,
+        };
+      }
+      return {
+        files: [...(state?.files || []), file],
+        activeEditor: file,
+        currentLanguage: file.language,
+        editorTabHistory: [...(state?.editorTabHistory || []),file]
+      };
+    }),
   deleteFile: (file) =>
     set((state) => ({
       files: state?.files?.filter(
@@ -38,8 +91,11 @@ export const useEditorFileStore = create<AllEditorFiles>()((set, get) => ({
             f.fileName === updatedFile ? { ...f, content: newContent } : f
           )
         : [],
-    })
-    ),
-    currentLanguage: '',
-    setCurrentLanguage: (lang) => set((state) => ({currentLanguage: lang}))
+    })),
+  currentLanguage: "",
+  setCurrentLanguage: (lang) => set({ currentLanguage: lang }),
+  setActiveEditor: (editor) =>
+    set({
+      activeEditor: editor,
+    }),
 }));
