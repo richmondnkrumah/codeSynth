a6b5369cc4c73fd9ba7d32407a2465e1363841f8
Void <richmondnkrumah04@gmail.com>
2024-02-19 12:22:57 +0000

    removed unused code

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState,useRef} from "react";
+import { useState} from "react";
 import Entry from "./Entry";
 import { useExplorerFileTree } from "@/store/ExplorerFileTree";
 
