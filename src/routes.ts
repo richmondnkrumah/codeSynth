f3c30d6feb6a5c4f178edb755d545775225de18f
Void <richmondnkrumah04@gmail.com>
2024-02-19 12:25:03 +0000

    api routes constant file

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+export const publicRoutes = [
+  "/"
+]
+
+
+// redirect to /settings
+export const authRoutes = [
+  "/auth/login",
+  "/auth/signup"
+]
+
+// api authentication prefix
+export const apiAuthPrefix = "/api/auth"
+
+// default redirect path after authentication
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
