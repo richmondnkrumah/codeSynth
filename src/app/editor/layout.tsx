e16623c6a3ba3028e31f8bf2fd9ae8ac3d80a092
Void <richmondnkrumah04@gmail.com>
2024-02-23 14:54:11 +0000

    removed cotributors parallel route

--- src/app/editor/layout.tsx
+++ src/app/editor/layout.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const EditorLayout = ({
   children,
-  contributors,
   explorer,
 }: {
   children: React.ReactNode;
-  contributors: React.ReactNode;
   explorer: React.ReactNode;
 }) => {
+
+  //TODO : add contributors as a collapsible panel to the explorer
+  //TODO : add a floating button for the preview site option at the right hand side of the editor
   return (
     <main className="w-screen h-screen relative">
       <div className="flex h-[calc(100vh-25px)] w-full">
-        <section className=" w-[250px]">{explorer}</section>
-        <div className="w-[calc(100%-300px)]">{children}</div>
-        <section className="hidden sm:block w-[50px]">{contributors}</section>
+        <section className="w-fit">{explorer}</section>
+        <div className="w-full overflow-hidden">{children}</div>
       </div>
       <StatusBar />
     </main>
