836952dbbdabae30301aba8b64ff23fe83615eb7
Void <richmondnkrumah04@gmail.com>
2024-02-23 14:55:51 +0000

    added drawer functionality with icons

--- src/app/editor/@explorer/page.tsx
+++ src/app/editor/@explorer/page.tsx
@@ -7,15 +7,38 @@ const Explorer = () => {
   const {getTheme} = useThemeStore();
   const currentTheme = getTheme();
 
+  const [openExplorer,setOpenExplorer] = React.useState(true)
+  const handleExplorer = () => {
+    setOpenExplorer(prev => !prev)
+  }
   return (
     <div
-      className={`${currentTheme.colors.secondary} h-full overflow-scroll pl-2`}
+      className={`${currentTheme.colors.primary} ${openExplorer ? 'w-[250px]' : 'w-[30px]'} h-full overflow-auto pl-2`}
     >
-      <div className="h-8">
-        <h2>CodeSynth</h2>
-      </div>
       
+      <div className="relative h-14 flex items-center justify-between pr-2">
+      <span className="absolute  bottom-1 bg-gray-500 w-[calc(100%_+_8px)] h-[3px] -left-2  "></span>
+        
+        {
+          openExplorer && <h2>CodeSynth</h2>
+        }
+        <div onClick={handleExplorer} className="w-5 h-5 ">
+          {
+            !openExplorer ? 
+            (
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.5 5L11.7929 11.2929C12.1834 11.6834 12.1834 12.3166 11.7929 12.7071L5.5 19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M13.5 5L19.7929 11.2929C20.1834 11.6834 20.1834 12.3166 19.7929 12.7071L13.5 19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            )
+            : 
+            (
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 19L12.7071 12.7071C12.3166 12.3166 12.3166 11.6834 12.7071 11.2929L19 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11 19L4.70711 12.7071C4.31658 12.3166 4.31658 11.6834 4.70711 11.2929L11 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            )
+          }
+        </div>
+      </div>
+      {
+        openExplorer && 
       <FileTree />     
+      }
     </div>
   );
 };
