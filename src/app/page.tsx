9778ee68db545efa7bddee01903dabe11150f31c
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-10-06 08:31:37 +0000

    updated hero header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const Home = (props: Props) => {
                   className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
 
                 >
-                  Easy and Simple Code Editing for Everyone
+                  Easy and Simple Code Editor for Everyone
                 </h1>
                 <div className="mx-auto max-w-3xl">
                   <p
