82a1c46153877f77e645e603a5fb55bce5095c8d
Void <richmondnkrumah04@gmail.com>
2025-05-27 00:30:28 +0000

    removed unused code

--- src/store/Theme.tsx
+++ src/store/Theme.tsx
@@ -1,8 +1,6 @@
 import {create} from 'zustand'
 import THEMES from '@/constant/themes'
 
-// safety purposes
-// "dark"|"light"|"test"|"red"|"green"|"purple"|"blue"|"orange"|"yellow"|"silver"|"teal"|"maroon"|"olive"|"navy"|"pink"|"cyan"|"coral"|"indigo"|"gold"|"salmon"|"sky-blue"|"dark-olive"|"orchid"|"lime"|"tan"|"cadet-blue"|"tomato"|"medium-slate-blue"|"light-coral"|"dark-khaki"|"slate-gray"|"sandy-brown"|"firebrick"|"plum"|"dark-cyan"|"thistle"|"medium-purple"|"dark-salmon"|"yellow-green"|"sienna"|"cadmium-yellow"|"rosy-brown"|"medium-aquamarine"|"indian-red"|"pale-violet-red"|"medium-sea-green"|"orange-red"|"dark-orchid"|"steel-blue"|"dark-green"|"light-pink"|"medium-spring-green"|"dark-turquoise"|"medium-violet-red"|"peru"|"chocolate"|"dark-olive-green"|"dark-magenta"|"dark-orange"|"dark-violet"|"deep-pink"|"firebrick"|"green-yellow"|"light-blue"|"light-green"|"light-salmon"|"navajo-white"|"royal-blue""dark"|"light"|"test"|"red"|"green"|"purple"|"blue"|"orange"|"yellow"|"silver"|"teal"|"maroon"|"olive"|"navy"|"pink"|"cyan"|"coral"|"indigo"|"gold"|"salmon"|"sky-blue"|"dark-olive"|"orchid"|"lime"|"tan"|"cadet-blue"|"tomato"|"medium-slate-blue"|"light-coral"|"dark-khaki"|"slate-gray"|"sandy-brown"|"firebrick"|"plum"|"dark-cyan"|"thistle"|"medium-purple"|"dark-salmon"|"yellow-green"|"sienna"|"cadmium-yellow"|"rosy-brown"|"medium-aquamarine"|"indian-red"|"pale-violet-red"|"medium-sea-green"|"orange-red"|"dark-orchid"|"steel-blue"|"dark-green"|"light-pink"|"medium-spring-green"|"dark-turquoise"|"medium-violet-red"|"peru"|"chocolate"|"dark-olive-green"|"dark-magenta"|"dark-orange"|"dark-violet"|"deep-pink"|"firebrick"|"green-yellow"|"light-blue"|"light-green"|"light-salmon"|"navajo-white"|"royal-blue"
 export type themesTypes = {
     name: string,
     colors: {
